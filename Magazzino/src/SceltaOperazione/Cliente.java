ba461eab3c16014e824d6325d98a6074cf19e6aa
Tino Bokoshev <bokoshevtino@gmail.com>
2023-11-01 15:29:18 +0100

    metodo ricerca per modello

--- Magazzino/src/SceltaOperazione/Cliente.java
+++ Magazzino/src/SceltaOperazione/Cliente.java
@@ -4,6 +4,7 @@ import Magazzino.*;
 import Prodotti.Prodotto;
 import Prodotti.TipoProdotto;
 import Ricerca.RicercaPerMarca;
+import Ricerca.RicercaPerModello;
 import Ricerca.RicercaPerTipo;
 
 import java.util.ArrayList;
@@ -14,7 +15,7 @@ public class Cliente{
 
         System.out.println("Quale azioni vuoi compiere?" +
                 "\n 1. Ricerca per tipo di prodotto" + "\n 2. Ricerca per Marca" + "\n 3. Ricerca per dimensione schermo" +
-                "\n 4. Ricerca per dimensione memoria");
+                "\n 4. Ricerca per dimensione memoria" + "\n 5. Ricerca per modello");
 
         int sceltaUtente = Input.readInt();
 
@@ -25,6 +26,10 @@ public class Cliente{
             }
             case 2: {
                 RicercaPerMarca.ricercaPerMarca(magazzino);
+                break;
+            }
+            case 5: {
+                RicercaPerModello.ricercaPerModello(magazzino);
                 break;
             }
 
