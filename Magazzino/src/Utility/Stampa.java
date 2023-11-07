97825e21d033d99a58e6469d8911e442401a3d56
Fabio-Frau <fbfr91@yahoo.it>
2023-11-07 19:03:39 +0100

    Aggiunta stampa formattata per cliente e gestore

--- Magazzino/src/Utility/Stampa.java
+++ Magazzino/src/Utility/Stampa.java
@@ -6,14 +6,29 @@ import java.util.ArrayList;
 
 public class Stampa {
 
-    public static void PerCliente(ArrayList<Prodotto> prodottoCercato){
-        StringBuilder stampaProdotto = new StringBuilder();
-        for (Prodotto prodotto : prodottoCercato){
-            stampaProdotto.append(prodotto.getProduttore()).append(' ').append(prodotto.getModello()).append("\t Dimensione Display(pollici): ")
-                    .append(prodotto.getDimDisplay()).append("\t Dimensione Memoria(GB): ").append(prodotto.getDimMemoria()).
-                    append("\t Prezzo: ").append(prodotto.getPrezzoVendita()).append(" € \n");
+    public static void PerCliente(ArrayList<Prodotto> prodottoCercato) {
+        System.out.printf("%-15s %-15s %-15s %-25s %-20s %-20s %-20s", "Tipo", "ID", "Produttore", "Modello", "Dim. Display",
+                "Dim. Memoria", "Prezzo Vendita");
+        System.out.println();
+        for (Prodotto prodotto : prodottoCercato) {
+            System.out.printf("%-15s %-15s %-15s %-25s %-20.1f %-20d %-20.2f", prodotto.getTipo(), prodotto.getId(), prodotto.getProduttore(),
+                    prodotto.getModello(), prodotto.getDimDisplay(), prodotto.getDimMemoria(),
+                    prodotto.getPrezzoVendita());
+            System.out.println();
         }
-        System.out.println(stampaProdotto);
+        System.out.println();
+    }
 
+    public static void PerGestore(ArrayList<Prodotto> prodottoCercato) {
+        System.out.printf("%-15s %-15s %-15s %-25s %-20s %-20s %-20s %-20s","Tipo", "ID", "Produttore", "Modello", "Dim. Display",
+                "Dim. Memoria", "Prezzo Acquisto", "Prezzo Vendita");
+        System.out.println();
+        for (Prodotto prodotto : prodottoCercato){
+            System.out.printf("%-15s %-15s %-15s %-25s %-20.1f %-20d %-20.2f %-20.2f", prodotto.getTipo(), prodotto.getId(), prodotto.getProduttore(),
+                    prodotto.getModello(), prodotto.getDimDisplay(), prodotto.getDimMemoria(),
+                    prodotto.getPrezzoAcquisto(), prodotto.getPrezzoVendita());
+            System.out.println();
+        }
+        System.out.println();
     }
 }
