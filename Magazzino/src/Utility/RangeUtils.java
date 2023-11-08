0981f5add960c596169e4b510630c05c74d5ef87
Valeria Parise <parisevaleria@live.com>
2023-11-08 17:20:41 +0100

    BigDouble

--- Magazzino/src/Utility/RangeUtils.java
+++ Magazzino/src/Utility/RangeUtils.java
@@ -1,13 +1,21 @@
 package Utility;
 
+import java.math.BigDecimal;
 import java.util.Arrays;
 
 public class Range {
-
     public static double[] DoubleRange(){
         double[] range = new double[2];
         for(int i = 0; i < range.length; i++){
             range[i] = Input.readInt();
+        }
+        Arrays.sort(range);
+        return range;
+    }
+    public static BigDecimal[] DoubleRangeBD(){
+        BigDecimal[] range = new BigDecimal[2];
+        for(int i = 0; i < range.length; i++){
+            range[i] = Input.readBigD();
         }
         Arrays.sort(range);
         return range;
