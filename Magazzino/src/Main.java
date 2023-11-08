d2d8f802f2091873d25937964aa17a2fd905fb04
Maria Grazia <mariagraziadante@gmail.com>
2023-11-08 10:40:01 +0100

    modifiche
    rinomina ListaClienti in LIstaUtenti e inserito lista

--- Magazzino/src/Main.java
+++ Magazzino/src/Main.java
@@ -1,3 +1,4 @@
+import Login.ListaUtenti;
 import Login.Utente;
 import Magazzino.*;
 import Prodotti.Prodotto;
@@ -21,21 +22,7 @@ public class Main {
 
         TipoUtente.sceltaUtente(prodottiPresenti);
 
-
-        ArrayList<Utente> listaUtenti = new ArrayList<>();
-        Utente utente1 = new Utente("Andrea", "Rossi", "andrearossi@gmail.com",
-                "andrea10", 349567742);
-        Utente utente2 = new Utente("Mario", "Bianchi", "mariobianchi@gmail.com",
-                "bianchi91", 342478902);
-        Utente utente3 = new Utente("Giulia", "Verdi", "giuliaverdi@gmail.com",
-                "giulietta12", 349721134);
-        Utente utente4 = new Utente("Sara", "Neri", "saraneri@gmail.com",
-                "sarabella1", 348334566);
-        listaUtenti.add(utente1);
-        listaUtenti.add(utente2);
-        listaUtenti.add(utente3);
-        listaUtenti.add(utente4);
-
+        ListaUtenti listaUtenti = new ListaUtenti();
 
 
 
