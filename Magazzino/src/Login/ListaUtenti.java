c6b82cb374fd82ce93c2ed20fe5e2d0c31609389
Valeria Parise <parisevaleria@live.com>
2023-11-08 11:33:52 +0100

    login

--- Magazzino/src/Login/ListaUtenti.java
+++ Magazzino/src/Login/ListaUtenti.java
@@ -4,8 +4,11 @@ import java.util.ArrayList;
 
 public class ListaUtenti {
     ArrayList<Utente> listaUtenti = new ArrayList<>();
+    public ArrayList <Utente> getListaUtenti(){return listaUtenti;}
 
-    public ListaUtenti() {
+    public void setLista(ArrayList<Utente> lista){this.listaUtenti = lista;}
+
+    public ArrayList<Utente> listaUtenti() {
         Utente utente1 = new Utente("Andrea", "Rossi", "andrearossi@gmail.com",
                 "andrea10", 349567742);
         Utente utente2 = new Utente("Mario", "Bianchi", "mariobianchi@gmail.com",
@@ -14,11 +17,12 @@ public class ListaUtenti {
                 "giulietta12", 349721134);
         Utente utente4 = new Utente("Sara", "Neri", "saraneri@gmail.com",
                 "sarabella1", 348334566);
-        listaUtenti.add(utente1);
-        listaUtenti.add(utente2);
-        listaUtenti.add(utente3);
-        listaUtenti.add(utente4);
+
+        this.listaUtenti.add(utente1);
+        this.listaUtenti.add(utente2);
+        this.listaUtenti.add(utente3);
+        this.listaUtenti.add(utente4);
+
+        return listaUtenti;
     }
-
-
 }
--- Magazzino/src/Login/Login.java
+++ Magazzino/src/Login/Login.java
@@ -1,8 +1,5 @@
 package Login;
 import Magazzino.Magazzino;
-import Prodotti.Prodotto;
-import javax.swing.text.Utilities;
-import java.util.ArrayList;
 
 public class Login {
 
@@ -26,11 +23,11 @@ public class Login {
         while (!isTrue){
             boolean f = false;
             System.out.println("Email:");
-            String mail = Utility.Input.readStr();
+            String email = Utility.Input.readStr();
             System.out.println("Password:");
             String psw = Utility.Input.readStr();
 
-                for (Utenti u : lista.getLista()){
+                for (Utente u : lista.getListaUtenti()){
                     if (u.getEmail().equals(email) && u.getPassword().equals(psw)) {
                     f = true;
                     Utente user = u;}
@@ -40,37 +37,25 @@ public class Login {
                      }
                 }
         }
-    public static ListaUtenit pgRegistrati(ListaUtenti lista){
+    public static Utente pgRegistrati(ListaUtenti lista){
         System.out.println("Nome Utente: ");
-        String name = Utility.Input.readStr();
+        String nome = Utility.Input.readStr();
         System.out.println("Cognome Utente: ");
         String cognome = Utility.Input.readStr();
         System.out.println("Email: ");
         String email = Utility.Input.readStr();
-        System.out.println("Username: ");
-        String username = Utility.Input.readStr();
         System.out.println("Password: ");
         String password = Utility.Input.readStr();
         System.out.println("Numero di telefono: ");
         int telefono = Utility.Input.readInt();
 
-        //Utente u1 = new Utente(****);
+        Utente u1 = new Utente(nome, cognome, email, password, telefono);
         return u1;
     }
 
-    public static ListaUtenti verificaPsw(ListaUtenti lista){
-        System.out.println("Email: ");
-        String email = Utility.Input.readStr();
-        if (verificaMail(list, mail)){
-            resetPsw(list,mail);
-        } else {
-            System.out.println("Email  non valida.");
-        }
-        return lista;
-    }
 
     public static boolean verificaMail(ListaUtenti lista, String mail){
-        for (Utenti u : lista.getLista()){
+        for (Utente u : lista.getListaUtenti()){
             if(u.getEmail().equals(mail)){
                 System.out.println("Email corretta");
                 return true;
@@ -79,13 +64,24 @@ public class Login {
         return false;
     }
 
-    public static ListaUtenti resetPsw (ListaUtenti lista, String main){
-        for (Utenti u : lista.getLista()){
+    public static ListaUtenti resetPsw (ListaUtenti lista, String mail){
+        for (Utente u : lista.getListaUtenti()){
             if (u.getEmail().equals(mail)){
-                System.out.println("Inserisci password: ");
+                System.out.println("Inserisci la nuova password: ");
                 String psw = Utility.Input.readStr();
                 u.setPassword(psw);
             }
+        }
+        return lista;
+    }
+
+    public static ListaUtenti verificaPsw(ListaUtenti lista){
+        System.out.println("Email: ");
+        String email = Utility.Input.readStr();
+        if (verificaMail(lista, email)){
+            resetPsw(lista,email);
+        } else {
+            System.out.println("Email non valida.");
         }
         return lista;
     }
